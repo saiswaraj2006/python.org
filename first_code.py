3327737a41d33fa43ed58e6daa61c0207075ebc9
Sai swaraj <saiswaraj2006@gmail.com>
2026-03-28 22:47:57 +0530

    Initial Commit

---
+++ first_code.py
@@ -0,0 +1,21 @@
+# this is my first code
+print("Hello World!")
+# be aware of the case sensitive while declaring the variables
+# variables must start with a letter or underscore(_)
+# don't start with digits (or) symbol
+# - Only letters, digits, and underscores allowed
+# -  ✓ student_1
+# -  ✕ student-name
+sai='10'
+SAI='20'
+print(sai)
+# finding location of a variable
+print(id(SAI))# output-2229120399280
+print(id(sai))# output-2229120397936
+python_marks1=18
+python_marks2=20
+print(python_marks2)#output-20
+
+
+
+
