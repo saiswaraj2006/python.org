b8c81178bcff0432df1ecbb886025a052052c3ac
Sai swaraj <saiswaraj2006@gmail.com>
2026-04-26 23:15:57 +0530

    ADDING THE REAL LIFE EXAMPLES

--- Nestedloops.py
+++ Nestedloops.py
@@ -66,8 +66,84 @@ print('*'*n)
 for i in range(n-2):#is number of middle rows 
     print('*'+' '*(n-2)+'*')#here in the middle (n-2) rows are printing with spaces
 print('*'*n)
+#SCHOOL TIMETABLE
+#Each school day has multiple periods
+days=['Monday','Tuesday','Wednesday']
+periods=['Mathematics','Physics','Chemistry']
+for day in days:
+    for period in periods:
+        print(f"{day}: {period}")
+'''
+output=Monday: Physics
+Monday: Chemistry
+Tuesday: Mathematics
+Tuesday: Physics
+Tuesday: Chemistry
+Wednesday: Mathematics
+Wednesday: Physics
+Wednesday: Chemistry
+'''
+#for example if i have a holiday at the Wednesday then
+print()
+for day in days:
+    if day=='Wednesday':
+        print(f"{day}: Holiday")
+        continue
+    for period in periods:
+        print(f"{day}: {period}")
+'''
+Monday: Mathematics
+Monday: Physics
+Monday: Chemistry
+Tuesday: Mathematics
+Tuesday: Physics
+Tuesday: Chemistry
+Wednesday: Holiday→→(here it prints  holiday so school is leave)
+'''
+size = 8
+
+for r in range(size):
+    for c in range(size):
+        if (r+c) % 2 == 0:
+            print("⬛", end=" ")
+        else:
+            print("⬜", end=" ")
+    print()
+print()
+
+rows = 7
+cols = 20
+
+# Define coordinates for each letter
+S_coords = {(0,1),(0,2),(0,3),(1,1),(2,1),(3,1),(3,2),(3,3),(4,3),(5,3),(6,1),(6,2),(6,3)}
+A_coords = {(0,6),(0,7),(0,8),(1,6),(1,8),(2,6),(2,7),(2,8),(3,6),(3,8),(4,6),(4,8),(5,6),(5,8),(6,6),(6,8)}
+I_coords = {(0,11),(0,12),(0,13),(1,12),(2,12),(3,12),(4,12),(5,12),(6,11),(6,12),(6,13)}
+
+for r in range(rows):
+    for c in range(cols):
+        if (r,c) in S_coords or (r,c) in A_coords or (r,c) in I_coords:
+            print("⬜", end=" ")
+        else:
+            print("⬛", end=" ")
+    print()
+'''i just created 'sai' by using the coordinates,the output is crazy
+⬛ ⬜ ⬜ ⬜ ⬛ ⬛ ⬜ ⬜ ⬜ ⬛ ⬛ ⬜ ⬜ ⬜ ⬛ ⬛ ⬛ ⬛ ⬛ ⬛ 
+⬛ ⬜ ⬛ ⬛ ⬛ ⬛ ⬜ ⬛ ⬜ ⬛ ⬛ ⬛ ⬜ ⬛ ⬛ ⬛ ⬛ ⬛ ⬛ ⬛ 
+⬛ ⬜ ⬛ ⬛ ⬛ ⬛ ⬜ ⬜ ⬜ ⬛ ⬛ ⬛ ⬜ ⬛ ⬛ ⬛ ⬛ ⬛ ⬛ ⬛ 
+⬛ ⬜ ⬜ ⬜ ⬛ ⬛ ⬜ ⬛ ⬜ ⬛ ⬛ ⬛ ⬜ ⬛ ⬛ ⬛ ⬛ ⬛ ⬛ ⬛ 
+⬛ ⬛ ⬛ ⬜ ⬛ ⬛ ⬜ ⬛ ⬜ ⬛ ⬛ ⬛ ⬜ ⬛ ⬛ ⬛ ⬛ ⬛ ⬛ ⬛ 
+⬛ ⬛ ⬛ ⬜ ⬛ ⬛ ⬜ ⬛ ⬜ ⬛ ⬛ ⬛ ⬜ ⬛ ⬛ ⬛ ⬛ ⬛ ⬛ ⬛ 
+⬛ ⬜ ⬜ ⬜ ⬛ ⬛ ⬜ ⬛ ⬜ ⬛ ⬛ ⬜ ⬜ ⬜ ⬛ ⬛ ⬛ ⬛ ⬛ ⬛ 
+'''
+#it prints white block in the mentioned coordinates other wise black so 
+#then after successful iteration till last coordinate it gives the above output
+
+#For printing the months in a year i also use the nested loop
+months=['January','February','March','April','May','June','July','August','September','October','November','December']
+numbers=[1,2,3,4,5,6,7,8,9,10,11,12]
+for no,month in zip(numbers,months):
+    if month=='April'and no==4:
+        print(f"{no}: {month}: (current month)")
+    else:
+        print(f"{no}: {month}")
     
-
-
-
-
