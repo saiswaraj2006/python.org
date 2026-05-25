08a527ad1d82ccbefdc38b2ea835387028d4cc15
Sai swaraj <saiswaraj2006@gmail.com>
2026-05-25 21:04:22 +0530

    Reading the modules concept

---
+++ Invitation.py
@@ -0,0 +1,3 @@
+#invitation to my friend
+def invite_to_my_Marriage(myname,receiver_name):
+    return f"Hello,im {myname} from kdm village. \nMy dear {receiver_name}  my brother is getting married on 20th day of this month so we'd love for you to join us in celebrating this special day."
---
+++ RE_Invitation.py
@@ -0,0 +1,4 @@
+import Invitation
+#above i have imported a file in this file for group together in a file 
+#hence this is called a module
+print(Invitation.invite_to_my_Marriage("Swaran","Rohan"))
