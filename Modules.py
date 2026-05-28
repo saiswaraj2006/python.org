e3f3dc64c8be4de5c809572539a57917a7a5336b
Sai swaraj <saiswaraj2006@gmail.com>
2026-05-28 22:22:10 +0530

    Adding the types of MODULES with examples

---
+++ Modules.py
@@ -0,0 +1,47 @@
+#A module in python is a file that contains python code-it can define functions, classes, and variables.
+#or
+#A Module is a .py file that groups related code together
+#it has built-in Modules like math,os,random etc..
+#example i performed "Invitation.py" file by importing it into "RE_Invitation.py"
+'''
+in this i declared a function that returns the invitation to my friend 
+in "Invitation.py then imported that file to "RE_Invitation.py then printed
+'''
+#above one is the example of the modules
+#here different types of modules in the python
+'''
+1. Built-in Modules
+ex:math, os ,sys ,random, datetime.
+
+2.User-Defined Modules
+ex: like recently created "eventmanager" with packages like guestlist.py,invitation.py, remainder.py.
+
+3.External Modules
+ex:numpy, pandas , flask.
+'''
+#1.Built-in Modules
+from math import pi
+print(pi)
+#output=3.141592653589793
+from math import sqrt as square_root
+print(square_root(576))
+#output=24.0
+
+from math import pow as power
+print(power(5,2))#returns:25.0
+print(power(23,3))#returns:12167.0
+
+#3.external modules
+import pandas as pd
+df = pd.DataFrame({"Name": ["Soumya", "Priya"], "Event": ["Marriage", "Marriage"]})
+print(df)
+#it returns 
+'''  Name     Event
+0  Soumya  Marriage
+1   Priya  Marriage
+'''
+from PIL import Image
+img = Image.open("photo1.jpeg")
+img = img.resize((200, 200))
+img.save("small_photo.jpeg")
+
