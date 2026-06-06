29277ce8100478c3e4913f9583885c2091ec1c0b
Sai swaraj <saiswaraj2006@gmail.com>
2026-06-06 22:21:59 +0530

    Solving the Pascals triangle problem

---
+++ pascals_triangle.py/triangle.py
@@ -0,0 +1,13 @@
+def pascals_triangle(n):
+    triangle=[]
+    if n<=0:
+        print("n value must be positive integer")
+    else:
+        for i in range(n):
+            row=[1]*(i+1)
+            for j in range(1,i):
+                row[j]=triangle[i-1][j-1]+triangle[i-1][j]
+            triangle.append(row) 
+    for row in triangle:
+        print(" ".join(map(str, row)))
+pascals_triangle(5)
