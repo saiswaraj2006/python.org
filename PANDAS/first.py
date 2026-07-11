fccf9aaef7c63300de0a1523d42e508c0ea3ae5c
Sai swaraj <saiswaraj2006@gmail.com>
2026-07-11 21:30:15 +0530

    Adding conditional and logical opreations in pandas

--- PANDAS/first.py
+++ PANDAS/first.py
@@ -97,3 +97,46 @@ Rohit     62000
 Kavya     46500
 Name: salaries, dtype: int64
 '''
+#logical operators
+#using and operator which is &
+
+print(s2[~(s2>50000)])
+'''
+Ramesh     4500
+Sneha     48000
+Meena     47500
+Vivek     50000
+Kavya     46500
+Name: salaries, dtype: int64'''
+
+print(s2[(s2>=50000) & (s2<60000)])
+'''
+Priya     52500
+Kiran     55000
+Vivek     50000
+Anjali    58000
+Name: salaries, dtype: int64'''
+#or=!
+
+#MODIFYING
+s2["Ramesh"]=45000
+print(s2)
+#now 4500 to 45000 it will be changed
+
+import numpy as np
+ser=pd.Series(['a',np.nan,1,np.nan,2])
+print(ser.notnull().sum())
+#3
+print(ser.notnull())
+'''
+0     True
+1    False
+2     True
+3    False
+4     True
+dtype: bool'''
+print(ser.isnull().sum())#2 
+#here actually the sum() is not adding the null numbers
+#in pandas it will give a boolean representation how many Trues it will be the answer 
+#Example in the above
+print(ser.isnull())
