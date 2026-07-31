9a387829e769c1b716ef1fd36144cc54d2155ef9
Sai swaraj <saiswaraj2006@gmail.com>
2026-07-31 23:00:47 +0530

    solving problem

--- OOPS/Problems.py
+++ OOPS/Problems.py
@@ -99,5 +99,43 @@ s1=Student("Ravi",40)
 s2=Student("Reshma",39)
 s1.student_details()
 s2.student_details()
-        
-    
+'''
+Ravi is Passed,and congras!
+Reshma is Fail,Best of Luck'''
+
+#problem
+'''
+Create a Rectangle class with attributes for length and width. Add methods to:
+Calculate the area
+Calculate the perimeter
+Compare two rectangles to see which one has a larger area'''
+class Rectangle:
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+    def perimeter(self):
+        return 2 * (self.length + self.width)
+
+    def compare_area(self, other_rectangle):
+        if self.area() > other_rectangle.area():
+            print("This rectangle has a larger area.")
+        elif self.area() < other_rectangle.area():
+            print("The other rectangle has a larger area.")
+        else:
+            print("Both rectangles have the same area.")
+
+
+
+rect1 = Rectangle(10, 5)
+rect2 = Rectangle(8, 7)
+print("Rectangle 1 Area:", rect1.area())
+print("Rectangle 1 Perimeter:", rect1.perimeter())
+print("Rectangle 2 Area:", rect2.area())
+print("Rectangle 2 Perimeter:", rect2.perimeter())
+
+# Compare areas
+rect1.compare_area(rect2)
