696bfe765f876d49b45dc3e487d70e0d9ff521be
Sai swaraj <saiswaraj2006@gmail.com>
2026-08-10 23:00:10 +0530

    Adding the deleting the head node

--- LinkedList/Example.py
+++ LinkedList/Example.py
@@ -56,4 +56,42 @@ ll.insert_at_beginning(30)
 ll.insert_at_beginning(20)
 ll.insert_at_beginning(10)
 ll.display()
+'''
+10 -> 20 -> 30 -> None
+'''
+#deleting the beginning (Head Removal)
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert_at_beginning(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def delete_at_beginning(self):
+        if self.head is None:
+            print("List is empty, nothing to delete.")
+            return
+        self.head = self.head.next   # move head to next node
+
+    def display(self):
+        temp = self.head
+        while temp:
+            print(temp.data, end=" -> ")
+            temp = temp.next
+        print("None")
+ll = LinkedList()
+ll.insert_at_beginning(30)
+ll.insert_at_beginning(20)
+ll.insert_at_beginning(10)
+
+ll.display()          # 10 -> 20 -> 30 -> None
+ll.delete_at_beginning()
+ll.display()          # 20 -> 30 -> None
 
