28a9984e62936ba51ed0537ec6471dbf9a044e98
Sai swaraj <saiswaraj2006@gmail.com>
2026-09-06 22:59:06 +0530

    performing operations on subplots

--- MATPLOTLIB/codes.py
+++ MATPLOTLIB/codes.py
@@ -256,6 +256,7 @@ plt.legend(loc="lower right",fontsize=12,title="exam results",bbox_to_anchor=(1.
 plt.title("Pie chart")
 plt.show()
 '''
+'''
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -278,5 +279,19 @@ plt.title("Day 3: Scatter Plot - Study vs Marks")
 plt.xlabel("Study Hours")
 plt.ylabel("Marks")
 plt.legend()
+plt.show()'''
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(0, 10, 100)
+y1 = np.sin(x)
+y2 = np.cos(x)
+# Create 2 subplots side by side
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 4))
+ax1.plot(x, y1, color="blue")
+ax1.set_title("Sine Wave")
+ax2.plot(x, y2, color="red")
+ax2.set_title("Cosine Wave")
+plt.tight_layout()
 plt.show()
 
